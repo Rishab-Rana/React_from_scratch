2456c2959feb77291225ae12595da8b2f763488e
Lucifer <rishabrana228@gmail.com>
2023-03-29 02:26:15 +0530

    basic of props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Welcome from './Component/welcome';
+import Hero from './Component/heros';
 
 function App() {
   return (
     <div className="App">
      <Welcome />
+    <Hero name="rishab" surname="Rana"/>
+    <Hero name="Rishi"/>
+    <Hero name="rishu"/>
     </div>
   );
 }
---
+++ src/Component/heros.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Hero = (props) => {
+    return <h1>{props.name} {props.surname}</h1>
+}
+
+
+export default Hero
