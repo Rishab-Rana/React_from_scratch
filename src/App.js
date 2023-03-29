83d94099ea38fafd7d98c284117b7f59bb7ccc38
Lucifer <rishabrana228@gmail.com>
2023-03-29 23:36:54 +0530

    learned more about state and useState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import Welcome from './Component/welcome';
-import Hero from './Component/heros';
+import Counter from './Component/Counter';
 
 function App() {
   return (
     <div className="App">
-     <Welcome />
-    <Hero name="rishab" surname="Rana"/>
-    <Hero name="Rishi"/>
-    <Hero name="rishu"/>
+      <Counter />
     </div>
   );
 }
